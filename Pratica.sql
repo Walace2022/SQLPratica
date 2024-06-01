6d9fe0656d2f93581eba721a4ea3dacec9003da7
Henrique Walace <henriquewalace@hotmail.com>
2024-06-01 15:22:48 -0300

    feat: Busca o filme De Volta para o Futuro

--- Pratica.sql
+++ Pratica.sql
@@ -6,3 +6,8 @@ SELECT Nome,Ano FROM Filmes
 
 SELECT Nome,Ano FROM Filmes
 ORDER BY Ano
+
+--Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+
+SELECT Nome,Ano,Duracao FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
