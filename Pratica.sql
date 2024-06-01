9615f81aad80625e592992bdf15d5fae43bff78c
Henrique Walace <henriquewalace@hotmail.com>
2024-06-01 15:24:18 -0300

    feat: Busca filmes do ano 1997

--- Pratica.sql
+++ Pratica.sql
@@ -11,3 +11,8 @@ ORDER BY Ano
 
 SELECT Nome,Ano,Duracao FROM Filmes
 WHERE Nome = 'De Volta para o Futuro'
+
+--Buscar os filmes lançados em 1997
+
+SELECT Nome,Ano,Duracao FROM Filmes
+WHERE Ano = 1997
