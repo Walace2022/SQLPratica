4a516d77623404a4fd91e83a3f10a9b1ffd1c976
Henrique Walace <henriquewalace@hotmail.com>
2024-06-01 15:31:26 -0300

    feat: Busca quantidade de filmes por ano ordenado decrescente

--- Pratica.sql
+++ Pratica.sql
@@ -27,3 +27,9 @@ WHERE Ano > 2000
 SELECT Nome,Ano,Duracao FROM Filmes
 WHERE Duracao > 100 AND Duracao < 150
 ORDER BY Duracao
+
+--Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela quantidade em ordem decrescente
+
+SELECT Ano,COUNT(*) Quantidade FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
