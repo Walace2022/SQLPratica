894a4840cb26922894f32c7abdda4d2a7482eb77
Henrique Walace <henriquewalace@hotmail.com>
2024-06-01 15:27:19 -0300

    feat: Busca filmes com duração entre 100 e 150 ordenados por duracao

--- Pratica.sql
+++ Pratica.sql
@@ -21,3 +21,9 @@ WHERE Ano = 1997
 
 SELECT Nome,Ano,Duracao FROM Filmes
 WHERE Ano > 2000
+
+--Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+
+SELECT Nome,Ano,Duracao FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
